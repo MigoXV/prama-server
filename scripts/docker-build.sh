37fb4cf4bf5ab4b8c376755686bc9c03ef624d3a
migo <1695587905@qq.com>
2026-06-12 18:30:21 +0800

    chore: 发布 0.10.0.a3-gz03 镜像版本

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-VERSION="${VERSION:-0.10.0.a2-gz03}"
+VERSION="${VERSION:-0.10.0.a3-gz03}"
 REGISTRY="${REGISTRY:-registry.cn-hangzhou.aliyuncs.com/migo-dl}"
 FRONTEND_IMAGE="${FRONTEND_IMAGE:-${REGISTRY}/prama-server-frontend:${VERSION}}"
 BACKEND_IMAGE="${BACKEND_IMAGE:-${REGISTRY}/prama-server-backend:${VERSION}}"
